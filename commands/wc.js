1da5d7a6bd6379803a55262abc4e8dbec4df4583
Naki <naki.md@gmail.com>
2019-04-15 23:48:29 +0200

    massive update

--- commands/wc.js
+++ commands/wc.js
@@ -1,30 +1,16 @@
+const Discord = require('discord.js')
 const worder = require("worder")
-
-const sortByFrequency = (array) => {
-	let frequency = {}
-	array.forEach((value) => { frequency[value] = 0 })
-	let uniques = array.filter((value) => { return ++frequency[value] == 1 })
-	return uniques.sort((a, b) => { return frequency[b] - frequency[a] })
-}
-
 const blockspring = require('blockspring')
 const svg_to_png = require('svg-to-png')
 const path = require('path')
- 
 
- 
 
 const wordcloud = async (text) => {
     return new Promise(async (resolve, reject) => {
         blockspring.runParsed("98203ea3afcaccbd3daf7b68e2183957", { "text": text, "width": 500, "height": 400, "rotate_flag": true, "min_freq": 0 }, { api_key: "br_108841_c340f1865696044fec6bfc698e1935bce126cbc5"}, async function (res) {
             const inputFilePath = res.params.image
-            
-            console.log(inputFilePath)
-
             await svg_to_png.convert(inputFilePath, "output")
-
             const outputFilePath = path.join(__dirname, "..", "output", path.basename(inputFilePath)).replace(/\.[^\.]+$/, '.png');
-            console.log(outputFilePath)
             return resolve(outputFilePath)
         })
     })
@@ -42,16 +28,13 @@ module.exports = {
         .then(wordcloud)
         .then((filename) => {
             console.log("filename = %s", filename)
-
-            //const embed = new Discord.RichEmbed()
-            //.setTitle(`Word cloud`)
-            //.setColor(0xFFFFFF)
-
-            ctx.msg.channel.send("embed", {
-                files: [
-                    filename
-                ]
-            })
+            
+            const embed = new Discord.RichEmbed()
+            .setTitle(`Word cloud`)
+            .setColor(0xFFFFFF)
+            .attachFile(filename)
+            
+            ctx.msg.channel.send(embed)
         })
     }
 }
