2861dcfe4c2f285d922398fa3980afdc086995af
Naki <naki.md@gmail.com>
2019-04-16 02:21:55 +0200

    massive update

--- commands/wc.js
+++ commands/wc.js
@@ -10,41 +10,35 @@ const wordcloud = async (text) => {
         blockspring.runParsed("98203ea3afcaccbd3daf7b68e2183957", { "text": text, "width": 500, "height": 400, "rotate_flag": true, "min_freq": 0 }, { api_key: "br_108841_c340f1865696044fec6bfc698e1935bce126cbc5"}, async function (res) {
             const inputFilePath = res.params.image
             await svg_to_png.convert(inputFilePath, "output")
-            const outputFilePath = path.join(__dirname, "..", "output", path.basename(inputFilePath)).replace(/\.[^\.]+$/, '.png');
+            const outputFilePath = path.join(__dirname, "..", "output", path.basename(inputFilePath)).replace(/\.[^\.]+$/, '.png')
             return resolve(outputFilePath)
         })
     })
 }
+
 
 module.exports = {
     doc: {
         description: "Wordcloud (WIP)",
         example: "wc number"
     },
-    fun: (ctx) => {
+    fun: async (ctx) => {
         const count = ctx.args[2]
+
+        const embed = new Discord.RichEmbed()
+        .setTitle(`>Loading Word cloud`)
+        .setColor(0xFF0000)
+
+        const message = await ctx.msg.channel.send(embed)
+
         ctx.msg.channel.fetchMessages({ limit: count })
         .then(messages => messages.map(m => m.content).map((content) => (worder(content))).flat().join(' '))
-        .then(async (text) => {
-
-            const embed = new Discord.RichEmbed()
-            .setTitle(`>Loading Word cloud`)
-            .setColor(0xFF0000)
-            
-            return [text, await ctx.msg.channel.send(embed)]
-        })
-        .then(async ([text, message]) => {
-            return [await wordcloud(text), await message]
-        })
-        .then(async ([filename, message]) => {
-            
-            const embed = new Discord.RichEmbed()
-            .setTitle(`Word cloud`)
-            .setColor(0xFFFFFF)
-            .attachFile(filename)
-            
-            await message.delete()
-            return ctx.msg.channel.send(embed)
+        .then(wordcloud)
+        .then(async (filename) => {
+            message.delete()
+            ctx.msg.channel.send("", { files: [
+                filename
+            ]})
         })
     }
 }
--- commands/yt.js
+++ commands/yt.js
@@ -5,31 +5,41 @@ module.exports = {
         description: "yt word",
         example: "Youtube Search",
     },
-    fun: (ctx) => {
+    fun: async (ctx) => {
         
         const opts = {
             maxResults: 1,
             key: ctx.env.YOUTUBE_API_KEY
         }
+
         
         const query = ctx.args.slice(1).join(" ")
+        const msg = await ctx.msg.channel.send("Searching `" + query + "`")
         
-        ytSearch(query, opts, (err, results) => {
+        ytSearch(query, opts, async (err, results) => {
             if (err)
             return console.error(err)
             
             const result = results[0]
-            
-            /*
-            const embed = new Discord.RichEmbed()
-            .setTitle(he.decode(result.title))
-            .setThumbnail(result.thumbnails.medium.url)
-            .setDescription(he.decode(result.description))
-            .setURL(result.link)
-            .setColor(0xEE0000)
-            */
 
-            ctx.msg.channel.send(result.link)
+            if (result) {
+                /*
+                const embed = new Discord.RichEmbed()
+                .setTitle(he.decode(result.title))
+                .setThumbnail(result.thumbnails.medium.url)
+                .setDescription(he.decode(result.description))
+                .setURL(result.link)
+                .setColor(0xEE0000)
+                */
+
+                msg.edit(result.link)
+            } else {
+                msg.edit("No result found for `" + query + "`")
+            }
+            
+            await ctx.msg.delete()
+
+            
             
         })
         
